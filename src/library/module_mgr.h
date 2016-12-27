7721d2170144dec9ed0bd73575bba3316c8fade6
Jared Roesch <roeschinc@gmail.com>
2016-12-26 16:23:36 -0800

    feat(*): automatic import of tools.native working

--- src/library/module_mgr.h
+++ src/library/module_mgr.h
@@ -73,6 +73,8 @@ public:
     std::tuple<std::string, module_src, time_t> load_module(module_id const & id, bool can_use_olean) override;
 };
 
+module_loader mk_loader(module_id const & cur_mod, std::vector<module_info::dependency> const & deps);
+
 class module_mgr {
     bool m_use_snapshots = false;
     bool m_save_olean = false;
