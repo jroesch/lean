7670844704c2d0cc6541afd9129bc3a2b6ca61a4
Jared Roesch <roeschinc@gmail.com>
2017-01-02 01:55:47 -0800

    fix(library/tools/native): fix compilation of nested recursors

--- library/init/meta/expr.lean
+++ library/init/meta/expr.lean
@@ -86,13 +86,6 @@ meta constant expr.lower_vars    : expr → nat → nat → expr
 /- (copy_pos_info src tgt) copy position information from src to tgt. -/
 meta constant expr.copy_pos_info : expr → expr → expr
 
-meta constant expr.is_internal_cnstr : expr → option unsigned
-meta constant expr.get_nat_value : expr → option nat
-
-meta constant expr.collect_univ_params : expr → list name
-/-- `occurs e t` returns `tt` iff `e` occurs in `t` -/
-meta constant expr.occurs        : expr → expr → bool
-
 namespace expr
 open decidable
 
