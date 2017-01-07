108d377872ced1e01f263c9ecd5b17b0c2b835f9
Jared Roesch <roeschinc@gmail.com>
2017-01-07 02:47:22 -0800

    feat(library/tools/native): sucessfully compile standard.so

--- src/frontends/lean/elaborator.cpp
+++ src/frontends/lean/elaborator.cpp
@@ -3814,7 +3814,7 @@ expr resolve_names(environment const & env, local_context const & lctx, expr con
     return resolve_names_fn(env, lctx)(e);
 }
 
-static vm_obj tactic_save_type_info(vm_obj const & _e, vm_obj const & ref, vm_obj const & _s) {
+vm_obj tactic_save_type_info(vm_obj const & _e, vm_obj const & ref, vm_obj const & _s) {
     expr const & e = to_expr(_e);
     tactic_state const & s = tactic::to_state(_s);
     if (!get_global_info_manager() || !get_pos_info_provider()) return tactic::mk_success(s);
