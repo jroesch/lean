25828509d080ac956fe76d781407ad1afcb0261d
Jared Roesch <roeschinc@gmail.com>
2017-02-17 01:39:03 -0800

    WIP

--- library/tools/llvm/default.lean
+++ library/tools/llvm/default.lean
@@ -14,6 +14,20 @@ meta constant module : Type
 /-- Creates a new LLVM module object -/
 meta constant module.new : string → io module
 
-meta constant module.write_to : string → module → io unit
+/-- Should we try to write some definitions this way?
+    Having names is nice documentation -/
+meta constant module.write_to :
+    forall (path : string), module → io unit
+
+/- An LLVM function object. -/
+meta constant function : Type
+
+meta constant module.get_function : module → string → option function
+
+meta constant function.new : module → string → io function
+
+meta constant basic_block : Type
+
+meta constant basic_block.new : string → option function → option basic_block → io basic_block
 
 end llvm
