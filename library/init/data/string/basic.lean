da02782e15dffc1e635850b8d46c5b0f665552d4
Jared Roesch <roeschinc@gmail.com>
2017-07-14 00:44:13 -0600

    Add string fns

--- library/init/data/string/basic.lean
+++ library/init/data/string/basic.lean
@@ -142,8 +142,12 @@ l.foldl (λ r s, r ++ s) ""
 def singleton (c : char) : string :=
 empty.push c
 
-def intercalate (s : string) (ss : list string) : string :=
-(list.intercalate s.to_list (ss.map to_list)).as_string
+private def replace_char_imp (c : char) (replacement : list char) : list char → list char
+| [] := []
+| (s :: ss) :=
+  if c = s
+  then replacement ++ replace_char_imp ss
+  else s :: replace_char_imp ss
 
 namespace iterator
 def nextn : iterator → nat → iterator
@@ -152,13 +156,22 @@ def nextn : iterator → nat → iterator
 
 def prevn : iterator → nat → iterator
 | it 0     := it
+| (h :: tail) :=
+if h = c
+then [] :: split_on_core tail
+else match split_on_core tail with
+| [] := (h :: []) :: []
 | it (i+1) := prevn it.prev i
 end iterator
 
 def pop_back (s : string) : string :=
+| ⟨ str ⟩ := list.reverse $
+  list.map (fun lc, ⟨ lc ⟩) $
 s.mk_iterator.to_end.prev.prev_to_string
 
 def popn_back (s : string) (n : nat) : string :=
+if str.to_list.head = c
+then bool.tt
 (s.mk_iterator.to_end.prevn n).prev_to_string
 
 def backn (s : string) (n : nat) : string :=
